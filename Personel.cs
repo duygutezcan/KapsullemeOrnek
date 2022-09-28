e1c43d17535695727dc826cd6ab93cd5948967ec
duygutezcan <duygutezcan9@gmail.com>
2022-09-28 22:36:49 +0300

    Add project files.

---
+++ Personel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeOrnek
+{
+    public class Personel
+    {
+        private string tckimlikno;
+        public string TCKİMLİKNO
+        {
+            get 
+            {
+                return tckimlikno.Substring(0,5)+"******";
+            }
+            set
+            {
+                bool kontrol = false;
+
+                if(value.Length == 11)
+                {
+                    for(int i=0; i<value.Length; i++)
+                    {
+                       bool sayiMi = char.IsNumber(value[i]); //123456789 
+                        if(sayiMi)
+                        {
+                            //yapmam gereken birşey yok
+                        }
+                        else
+                        {
+                            kontrol = true;
+                            break;
+                        }
+                    }
+                    if(kontrol)
+                    {
+                        Console.WriteLine("tc de geçersiz karakter var");
+                    }
+                    else
+                    {
+                        tckimlikno = value;
+                    }
+                  
+                }
+                else
+                {
+                    Console.WriteLine("girdiğiniz tc 11 karakter değil");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel P1 = new Personel();
+            P1.TCKİMLİKNO = "14569875655";
+            Console.WriteLine("tc kimlik numaranız: " +P1.TCKİMLİKNO);
+        }
+    }
+}
